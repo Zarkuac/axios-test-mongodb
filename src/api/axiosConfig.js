4be6b6f8ba541a1bb6488103a42581974200a6f7
ZARKUAC <tsotne.zarkua.1@btu.edu.ge>
2023-02-14 22:53:46 -0800

    hel

---
+++ src/api/axiosConfig.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL:'https://cloud.mongodb.com/v2/63e5f370bdb1165a04431f66',
+    headers: {"ngrok-skip-browser-warning": "true"}
+});
+
+
+
+//FKING AXIOSSSSSSSSSSSs
